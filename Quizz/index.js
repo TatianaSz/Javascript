cf2f90aff353f6e7c634f21c574b8ea54215a43a
Tatiana <Tatiszulik777@gmail.com>
2021-07-31 15:37:55 +0200

    changed timer

--- Quizz/index.js
+++ Quizz/index.js
@@ -1,15 +1,22 @@
 const options = document.getElementsByClassName("quizz-option");
-let i=3;
+let i=2;
 let inter;
 const firstPage= document.getElementById("one");
 const timer = document.getElementById("timer");
+const question = document.getElementById("quiz-question");
+const answers = document.getElementById("quiz-answers");
+
+function showAnswers(){
+console.log("answers")
+}
 
 function timerFunc(){
     timer.children[0].innerHTML=i;
-    console.log(timer.children[0]);
     i--;
     if(i<0){
+        timer.children[0].innerHTML="GO!";
         clearInterval(inter);
+        setTimeout(function(){ timer.style.display="none"; showAnswers()}, 1000);
     }
 }
 function countTime(){
@@ -17,3 +24,6 @@ inter = window.setInterval(timerFunc, 1000);}
 
 [...options].forEach(opt=>{opt.addEventListener("click", ()=>{firstPage.style.display="none"; timer.style.display="block"; countTime()})});
 
+const htmlQuestions=[{question:"test question", a:"answeA", b:"answerB", c:"answerC", d:"answerD"}];
+const cssQuestions=[];
+const jsQuestions=[];
