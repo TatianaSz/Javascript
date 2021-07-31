003e552802f53524401e246312223e0eec4e34db
Tatiana <Tatiszulik777@gmail.com>
2021-07-31 19:07:25 +0200

    added a function generating an array of unique and random questions

--- Quizz/index.js
+++ Quizz/index.js
@@ -1,23 +1,44 @@
 const options = document.getElementsByClassName("quizz-option");
 let i=2;
 let inter;
+let pointCount=0;
 const firstPage= document.getElementById("one");
 const timer = document.getElementById("timer");
 const question = document.getElementById("quiz-question");
 const answers = document.getElementById("quiz-answers");
 //quenstions and answers
-const htmlQuestions=[{question:"test question", 0:"answeA", 1:"answerB", 2:"answerC", 3:"answerD"}];
+const htmlQuestions=[{question:"test question", 0:"answeA", 1:"answerB", 2:"answerC", 3:"answerD", correrct:2}];
 const cssQuestions=[];
 const jsQuestions=[];
 // ------------------
+function randomQuestions(array) {
+    var i = array.length,
+        j = 0,
+        temp;
+    while (i--) {
+        j = Math.floor(Math.random() * (i+1));
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array.slice(0,10);
+}
+//-------------------
+
+
+
 function showAnswers(){
     question.style.opacity="1";
 question.textContent= htmlQuestions[0].question;
 for(let i = 0; i<4;i++){
    [...answers.children][i].textContent=htmlQuestions[0][i];
+   [...answers.children][i].addEventListener("click", ()=>{
+    i==htmlQuestions[0].correrct?pointCount++:console.log("false");
+    })
 }
-;
 }
+
+
 
 function timerFunc(){
     timer.children[0].innerHTML=i;
