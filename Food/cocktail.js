17a15a1651bf0b988eef28a268cb4dc848fb987e
Tatiana <Tatiszulik777@gmail.com>
2021-08-03 21:55:10 +0200

    changing the icon on cilck

--- Food/cocktail.js
+++ Food/cocktail.js
@@ -1,19 +1,25 @@
 
 
 let drinks=["Mojito"];
-let drinksPic=["url('./images/mojito.svg')"];
+let drinksPic=["url('./images/mojito.svg')","url('./images/round.svg')","url('./images/triangle.svg')","url('./images/cham.svg')"];
 
 let drinkImg = document.getElementsByClassName("drink-image")[0];
-console.log(drinkImg);
+let alc = document.getElementsByClassName("alcohol");
 drinkImg.style.backgroundImage=drinksPic[0];
 let nav = document.getElementById("nav");
 
 
+[...alc].forEach((al,i)=>al.addEventListener("click", function(){
+    console.log(i)
+    drinkImg.style.backgroundImage=drinksPic[i];
+}))
 
-
-async function getDrink(){
-    const drink = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita');
+async function getDrink(name){
+    const drink = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=`+name);
     const drinkData = await drink.json();
-    
+    console.log(drinkData)
     }
-    getDrink()
+    getDrink("mojito")
+    //strDrink - name of drink
+    //strIngredient1,2,3.... - ingredients
+    //strInstructions - instructions
