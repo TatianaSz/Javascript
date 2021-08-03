42d7be3bf5f2e59695f6c9c3dd9b7f984ca4a6db
Tatiana <Tatiszulik777@gmail.com>
2021-08-03 11:57:21 +0200

    added first styling and created my own svg

--- Food/cocktail.js
+++ Food/cocktail.js
@@ -1,6 +1,19 @@
+
+
+let drinks=["Mojito"];
+let drinksPic=["url('./images/mojito.svg')"];
+
+let drinkImg = document.getElementsByClassName("drink-image")[0];
+console.log(drinkImg);
+drinkImg.style.backgroundImage=drinksPic[0];
+let nav = document.getElementById("nav");
+
+
+
+
 async function getDrink(){
-const drink = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita');
-const drinkData = await drink.json();
-console.log(drinkData)
-}
-getDrink()
+    const drink = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita');
+    const drinkData = await drink.json();
+    
+    }
+    getDrink()
