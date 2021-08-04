1f8045cd4f6bf0d7cc5e66db991e0eb0c578f35c
Tatiana <Tatiszulik777@gmail.com>
2021-08-04 10:18:12 +0200

    added all gradients

--- Food/cocktail.js
+++ Food/cocktail.js
@@ -1,17 +1,44 @@
 
 
-let drinks=["Mojito"];
-let drinksPic=["url('./images/mojito.svg')","url('./images/round.svg')","url('./images/triangle.svg')","url('./images/cham.svg')"];
+const drinksPic=["url('./images/mojito.svg')","url('./images/triangle.svg')","url('./images/cham.svg')","url('./images/round.svg')"];
+const backstyles = [{
+    background: "rgb(208,251,44)",
+    background: "-moz-radial-gradient(circle at 100% 50%, rgba(208,251,44,1) 0%, rgba(114,255,27,1) 55%, rgba(147,255,150,1) 92%, rgba(205,235,220,1) 100%)",
+    background: "-webkit-radial-gradient(circle at 100% 50%, rgba(208,251,44,1) 0%, rgba(114,255,27,1) 55%, rgba(147,255,150,1) 92%, rgba(205,235,220,1) 100%)",
+    background: "radial-gradient(circle at 100% 50%, rgba(208,251,44,1) 0%, rgba(114,255,27,1) 55%, rgba(147,255,150,1) 92%, rgba(205,235,220,1) 100%)",
+    },
+    {
+    background: "rgb(238,150,75)",
+    background: "-moz-radial-gradient(circle at 70% 50%, rgba(238,150,75,1) 0%, rgba(238,66,102,1) 32%, rgba(239,118,122,1) 57%, rgba(246,231,203,1) 100%)",
+    background: "-webkit-radial-gradient(circle at 70% 50%, rgba(238,150,75,1) 0%, rgba(238,66,102,1) 32%, rgba(239,118,122,1) 57%, rgba(246,231,203,1) 100%)",
+    background: "radial-gradient(circle at 70% 50%, rgba(238,150,75,1) 0%, rgba(238,66,102,1) 32%, rgba(239,118,122,1) 57%, rgba(246,231,203,1) 100%)",
+},
+    {
+    background: "rgb(251,25,29)",
+    background: "-moz-radial-gradient(circle, rgba(251,25,29,1) 0%, rgba(244,93,1,1) 27%, rgba(255,218,34,1) 64%, rgba(255,254,185,1) 100%)",
+    background: "-webkit-radial-gradient(circle, rgba(251,25,29,1) 0%, rgba(244,93,1,1) 27%, rgba(255,218,34,1) 64%, rgba(255,254,185,1) 100%)",
+    background: "radial-gradient(circle, rgba(251,25,29,1) 0%, rgba(244,93,1,1) 27%, rgba(255,218,34,1) 64%, rgba(255,254,185,1) 100%)",
+    },
+    {
+    background: "rgb(168,202,223)",
+    background: "-moz-radial-gradient(circle at 90% 50%, rgba(168,202,223,1) 0%, rgba(42,157,143,1) 45%, rgba(60,163,154,1) 81%, rgba(97,139,74,1) 100%)",
+    background: "-webkit-radial-gradient(circle at 90% 50%, rgba(168,202,223,1) 0%, rgba(42,157,143,1) 45%, rgba(60,163,154,1) 81%, rgba(97,139,74,1) 100%)",
+    background: "radial-gradient(circle at 90% 50%, rgba(168,202,223,1) 0%, rgba(42,157,143,1) 45%, rgba(60,163,154,1) 81%, rgba(97,139,74,1) 100%)",
+    }];
 
-let drinkImg = document.getElementsByClassName("drink-image")[0];
-let alc = document.getElementsByClassName("alcohol");
+const drinkImg = document.getElementsByClassName("drink-image")[0];
+
+const alc = document.getElementsByClassName("alcohol");
 drinkImg.style.backgroundImage=drinksPic[0];
 let nav = document.getElementById("nav");
 
+const setStylesOnElement = function(styles, element){
+    Object.assign(element.style, styles);
+};
 
 [...alc].forEach((al,i)=>al.addEventListener("click", function(){
-    console.log(i)
     drinkImg.style.backgroundImage=drinksPic[i];
+    setStylesOnElement(backstyles[i],document.getElementsByClassName("gradient")[0])
 }))
 
 async function getDrink(name){
